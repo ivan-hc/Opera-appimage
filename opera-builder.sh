1915208064d846fe8d199918a1e810ca5a6396c8
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-07-10 16:15:17 +0200

    Add files via upload

--- opera-builder.sh
+++ opera-builder.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
+# DOWNLOAD EXTRA-LIBS
+mkdir lib_extra
+cd ./lib_extra
+mkdir tmp
+cd ./tmp
+wget $(wget -q https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest -O - | grep browser_download_url | grep -i linux | grep -i 64 | cut -d '"' -f 4 | head -1)
+unzip ./*.zip
+cd ..
+mv ./tmp/libffmpeg.so ./libffmpeg.so
+
+mkdir tmp2
+cd ./tmp2
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ar x ./*.deb
+tar xf ./data.tar.xz
+cd ..
+mv ./tmp2/opt/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so ./libwidevinecdm.so
+
+rm -R -f ./tmp*
+chmod a+x ./*.so
+
+cd ..
+
+# CREATE OPERA STABLE APPIMAGE
 APP=opera
 mkdir tmp
+cp -r ./lib_extra ./tmp/lib_extra
 cd ./tmp
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage -O appimagetool
 chmod a+x ./appimagetool
 
-VERSION=$(wget -q "https://get.geo.opera.com/pub/opera/desktop/" -O - | grep href | tail -n 1 | cut -d '"' -f 2 | cut -d / -f 1)
-wget "https://get.geo.opera.com/pub/opera/desktop/$VERSION/linux/opera-stable_${VERSION}_amd64.deb"
+DEB=$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')
+wget https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/"$DEB"
 ar x ./*.deb
 tar xf ./data.tar.xz
 mkdir $APP.AppDir
@@ -15,6 +40,8 @@ mv ./usr/lib/x86_64-linux-gnu/opera/* ./$APP.AppDir/
 mv ./usr/share/applications/*.desktop ./$APP.AppDir/
 sed -i -e '/TargetEnvironment/d' ./$APP.AppDir/*.desktop
 mv ./usr/share/pixmaps/* ./$APP.AppDir/
+tar xf ./control.tar.xz
+VERSION=$(cat ./control | grep -i version | cut -c 10-)
 
 cat >> ./$APP.AppDir/AppRun << 'EOF'
 #!/bin/sh
@@ -26,12 +53,80 @@ exec "${HERE}"/$APP "$@"
 EOF
 chmod a+x ./$APP.AppDir/AppRun
 
-# ADD EXTRA LIBS FROM https://github.com/swanux/opera_codecs
-mkdir ./$APP.AppDir/lib_extra
-wget https://github.com/swanux/opera_codecs/raw/master/DEV_FILES/opt/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so -O ./$APP.AppDir/lib_extra/libwidevinecdm.so
-wget https://github.com/swanux/opera_codecs/raw/master/DEV_FILES/usr/lib/x86_64-linux-gnu/opera/lib_extra/libffmpeg.so -O ./$APP.AppDir/lib_extra/libffmpeg.so
-chmod a+x ./$APP.AppDir/lib_extra/*.so
+mv ./lib_extra ./$APP.AppDir/lib_extra
 
 ARCH=x86_64 ./appimagetool -n ./$APP.AppDir
 cd ..
-mv ./tmp/*AppImage ./Opera-Web-Browser-$VERSION-x86_64.AppImage
+mv ./tmp/*AppImage ./Opera-Web-Browser-"$VERSION"-x86_64.AppImage
+
+# CREATE OPERA BETA APPIMAGE
+APP=opera
+mkdir tmp2
+cp -r ./lib_extra ./tmp2/lib_extra
+cd ./tmp2
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage -O appimagetool
+chmod a+x ./appimagetool
+
+DEB=$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/opera-beta/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')
+wget https://deb.opera.com/opera-stable/pool/non-free/o/opera-beta/"$DEB"
+ar x ./*.deb
+tar xf ./data.tar.xz
+mkdir $APP.AppDir
+mv ./usr/lib/x86_64-linux-gnu/opera-beta/* ./$APP.AppDir/
+mv ./usr/share/applications/*.desktop ./$APP.AppDir/
+sed -i -e '/TargetEnvironment/d' ./$APP.AppDir/*.desktop
+mv ./usr/share/pixmaps/* ./$APP.AppDir/
+tar xf ./control.tar.xz
+VERSION=$(cat ./control | grep -i version | cut -c 10-)
+
+cat >> ./$APP.AppDir/AppRun << 'EOF'
+#!/bin/sh
+APP=opera-beta
+HERE="$(dirname "$(readlink -f "${0}")")"
+export UNION_PRELOAD="${HERE}"
+export LD_LIBRARY_PATH=/lib/:/lib64/:/lib/x86_64-linux-gnu/:/usr/lib/:"${HERE}"/lib_extra/:LD_LIBRARY_PATH
+exec "${HERE}"/$APP "$@"
+EOF
+chmod a+x ./$APP.AppDir/AppRun
+
+mv ./lib_extra ./$APP.AppDir/lib_extra
+
+ARCH=x86_64 ./appimagetool -n ./$APP.AppDir
+cd ..
+mv ./tmp2/*AppImage ./Opera-Web-Browser-BETA-"$VERSION"-x86_64.AppImage
+
+# CREATE OPERA DEVELOPER APPIMAGE
+APP=opera
+mkdir tmp3
+cp -r ./lib_extra ./tmp3/lib_extra
+cd ./tmp3
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage -O appimagetool
+chmod a+x ./appimagetool
+
+DEB=$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/opera-developer/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')
+wget https://deb.opera.com/opera-stable/pool/non-free/o/opera-developer/"$DEB"
+ar x ./*.deb
+tar xf ./data.tar.xz
+mkdir $APP.AppDir
+mv ./usr/lib/x86_64-linux-gnu/opera-developer/* ./$APP.AppDir/
+mv ./usr/share/applications/*.desktop ./$APP.AppDir/
+sed -i -e '/TargetEnvironment/d' ./$APP.AppDir/*.desktop
+mv ./usr/share/pixmaps/* ./$APP.AppDir/
+tar xf ./control.tar.xz
+VERSION=$(cat ./control | grep -i version | cut -c 10-)
+
+cat >> ./$APP.AppDir/AppRun << 'EOF'
+#!/bin/sh
+APP=opera-developer
+HERE="$(dirname "$(readlink -f "${0}")")"
+export UNION_PRELOAD="${HERE}"
+export LD_LIBRARY_PATH=/lib/:/lib64/:/lib/x86_64-linux-gnu/:/usr/lib/:"${HERE}"/lib_extra/:LD_LIBRARY_PATH
+exec "${HERE}"/$APP "$@"
+EOF
+chmod a+x ./$APP.AppDir/AppRun
+
+mv ./lib_extra ./$APP.AppDir/lib_extra
+
+ARCH=x86_64 ./appimagetool -n ./$APP.AppDir
+cd ..
+mv ./tmp3/*AppImage ./Opera-Web-Browser-DEV-"$VERSION"-x86_64.AppImage
