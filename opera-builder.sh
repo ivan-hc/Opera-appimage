365c4f1b5dc27b6196824aed2f81df6c4b0709ad
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-11 17:52:49 +0100

    Update opera-builder.sh

--- opera-builder.sh
+++ opera-builder.sh
@@ -8,42 +8,42 @@ cd ./tmp || exit 1
 
 # DOWNLOAD APPIMAGETOOL
 if ! test -f ./appimagetool; then
-	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool || exit 1
 	chmod a+x ./appimagetool
 fi
 
 # DOWNLOAD WIDEVINE
 if ! test -f ./*.deb; then
-	wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb || exit 1
 	ar x ./*.deb
 	tar xf ./data.tar.xz
-	mv ./opt/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so ./libwidevinecdm.so
+	mv ./opt/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so ./libwidevinecdm.so || exit 1
 fi
 
 # DOWNLOAD A WORKING VERSION OF FFMPEG (THE ONE IN OPERA IS BROKEN...FROM YEARS)
 if ! test -f ./*.zip; then
-	wget "$(wget -q https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest -O - | grep browser_download_url | grep -i linux | grep -i 64 | cut -d '"' -f 4 | head -1)"
-	unzip ./*.zip
+	wget "$(curl -Ls https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest | sed 's/[()",{} ]/\n/g' | grep -oi "https.*linux.*64.*zip$" | head -1)" || exit 1
+	unzip ./*.zip || exit 1
 fi
 
 # CREATE OPERA BROWSER APPIMAGES
 
 _create_opera_appimage(){
 	if wget --version | head -1 | grep -q ' 1.'; then
-		wget -q --no-verbose --show-progress --progress=bar "https://deb.opera.com/opera-stable/pool/non-free/o/$APP-$CHANNEL/$(wget -q https://deb.opera.com/"$APP"-"$CHANNEL"/pool/non-free/o/"$APP"-"$CHANNEL"/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')"
+		wget -q --no-verbose --show-progress --progress=bar "https://deb.opera.com/opera-stable/pool/non-free/o/$APP-$CHANNEL/$(wget -q https://deb.opera.com/"$APP"-"$CHANNEL"/pool/non-free/o/"$APP"-"$CHANNEL"/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')" || exit 1
 	else
-		wget "https://deb.opera.com/opera-stable/pool/non-free/o/$APP-$CHANNEL/$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/"$APP"-"$CHANNEL"/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')"
+		wget "https://deb.opera.com/opera-stable/pool/non-free/o/$APP-$CHANNEL/$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/"$APP"-"$CHANNEL"/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')" || exit 1
 	fi
 	ar x ./*.deb
 	tar xf ./data.tar.xz
 	mkdir "$APP".AppDir
-	mv ./usr/lib/x86_64-linux-gnu/oper*/* ./"$APP".AppDir/
-	mv ./usr/share/applications/*.desktop ./"$APP".AppDir/
+	mv ./usr/lib/x86_64-linux-gnu/oper*/* ./"$APP".AppDir/ || exit 1
+	mv ./usr/share/applications/*.desktop ./"$APP".AppDir/ || exit 1
 	sed -i -e '/TargetEnvironment/d' ./"$APP".AppDir/*.desktop
-	mv ./usr/share/pixmaps/* ./"$APP".AppDir/
-	cp ../libwidevinecdm.so ./"$APP".AppDir/
-	mv ./"$APP".AppDir/libffmpeg.so ./"$APP".AppDir/libffmpeg.so.old
-	cp ../libffmpeg.so ./"$APP".AppDir/
+	mv ./usr/share/pixmaps/* ./"$APP".AppDir/ || exit 1
+	cp ../libwidevinecdm.so ./"$APP".AppDir/ || exit 1
+	mv ./"$APP".AppDir/libffmpeg.so ./"$APP".AppDir/libffmpeg.so.old || exit 1
+	cp ../libffmpeg.so ./"$APP".AppDir/ || exit 1
 	tar xf ./control.tar.xz
 	VERSION=$(cat control | grep Version | cut -c 10-)
 
@@ -68,19 +68,19 @@ _create_opera_appimage(){
 CHANNEL="stable"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_opera_appimage
-cd ..
+cd .. || exit 1
 mv ./"$CHANNEL"/*.AppImage* ./
 
 CHANNEL="beta"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_opera_appimage
-cd ..
+cd .. || exit 1
 mv ./"$CHANNEL"/*.AppImage* ./
 
 CHANNEL="developer"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_opera_appimage
-cd ..
+cd .. || exit 1
 mv ./"$CHANNEL"/*.AppImage* ./
 
 cd ..
